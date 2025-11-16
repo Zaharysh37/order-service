560a260927c0a3eb4f3cb090f314cf295de6a113
MegaXRL <zaxarrim@gmail.com>
2025-11-16 20:58:42 +0300

    "db schema and entities"

---
+++ src/main/java/com/innowise/orderservice/core/entity/Order.java
@@ -0,0 +1,53 @@
+package com.innowise.orderservice.core.entity;
+
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.Table;
+import java.time.Instant;
+import java.util.Set;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "orders")
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Status status;
+
+    @Column(name = "creation_date", nullable = false, updatable = false)
+    private Instant creationDate;
+
+    @Column(name = "user_id", nullable = false)
+    private Long userId;
+
+    @OneToMany(
+        mappedBy = "order",
+        cascade = CascadeType.ALL,
+        orphanRemoval = true,
+        fetch = FetchType.LAZY
+    )
+    private Set<OrderItem> orderItems;
+
+    @PrePersist
+    protected void onCreate() {
+        this.creationDate = Instant.now();
+    }
+}
